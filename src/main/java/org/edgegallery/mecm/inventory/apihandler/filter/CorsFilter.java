30f7dc25b8fc9bc703ecbebc46fd4c0767eb5e06
shashikanth.vh@huawei.com <shashikanth.vh@huawei.com>
2020-09-13 02:06:00 +0530

    cors isue fix

--- src/main/java/org/edgegallery/mecm/inventory/apihandler/filter/CorsFilter.java
+++ src/main/java/org/edgegallery/mecm/inventory/apihandler/filter/CorsFilter.java
@@ -26,9 +26,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CorsFilter.class);
