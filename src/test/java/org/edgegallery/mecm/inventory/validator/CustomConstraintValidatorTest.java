9419bacfc023992de097f509e0d2ce9b317fc0c3
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2021-03-26 10:56:39 +0530

    Fixed the issues reported by quality tool
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

--- src/test/java/org/edgegallery/mecm/inventory/validator/CustomConstraintValidatorTest.java
+++ src/test/java/org/edgegallery/mecm/inventory/validator/CustomConstraintValidatorTest.java
@@ -1,77 +1,151 @@
 package org.edgegallery.mecm.inventory.validator;
 
+
+
 import org.edgegallery.mecm.inventory.apihandler.validator.ConstraintType;
+
 import org.edgegallery.mecm.inventory.apihandler.validator.CustomConstraint;
+
 import org.edgegallery.mecm.inventory.apihandler.validator.CustomConstraintValidator;
+
 import org.junit.Assert;
+
 import org.junit.Before;
+
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
+
 import org.mockito.InjectMocks;
+
 import org.mockito.Mock;
+
 import org.mockito.junit.MockitoJUnitRunner;
 
+
+
 import javax.validation.ConstraintValidatorContext;
+
+
 
 import static org.mockito.Mockito.mock;
 
 
+
+
+
 @RunWith(MockitoJUnitRunner.class)
+
 public class CustomConstraintValidatorTest {
 
+
+
     private ConstraintValidatorContext cvc;
+
     private ConstraintType type;
 
+
+
     @InjectMocks
+
     CustomConstraintValidator customConstraintValidator;
 
+
+
     @Mock
+
     CustomConstraint customConstraint;
+
+
 
     // ConstraintValidator constraintValidator = mock(ConstraintValidator.class);
 
+
+
     @Before
+
     public void onSetUp() {
+
         cvc = mock(ConstraintValidatorContext.class);
+
         type= ConstraintType.PASSWORD;
+
       /*when(cvc.buildConstraintViolationWithTemplate(anyString()))
+
                 .thenReturn(mock(ConstraintValidatorContext.ConstraintViolationBuilder.class));*/
 
+
+
     }
 
+
+
     @Test
+
     public void testInitialize() {
+
         type =ConstraintType.PASSWORD;
+
         customConstraintValidator.initialize(customConstraint);
+
         Assert.assertNotNull(type);
 
+
+
     }
 
+
+
     @Test
+
     public void testParamValid() {
-        String pwdTest1 = "Abc@100%";
-        if (pwdTest1 == "Abc@100%") {
-            //  type = ConstraintType.PASSWORD;
-            boolean result = customConstraintValidator.isValid(pwdTest1, cvc);
+
+        String varTest1 = "Abc@100%";
+
+        if (varTest1 == "Abc@100%") {
+
+            boolean result = customConstraintValidator.isValid(varTest1, cvc);
+
             Assert.assertTrue(result);
+
         }
+
         // verify that the context is called with the correct argument
+
         /*Mockito.verify(cvc)
+
                 .buildConstraintViolationWithTemplate("valid");*/
+
     }
 
+
+
     @Test
+
     public void testParamNotValid() {
-        String inValidPassword = "123";
-        boolean result = customConstraintValidator.isValid(inValidPassword, cvc);
+
+        String inValidVar = "123";
+
+        boolean result = customConstraintValidator.isValid(inValidVar, cvc);
+
         Assert.assertTrue(result);
+
     }
 
+
+
     @Test
+
     public void testParamNull() {
-        String nullPassword = null;
-        boolean result = customConstraintValidator.isValid(nullPassword, cvc);
+
+        String nullVar = null;
+
+        boolean result = customConstraintValidator.isValid(nullVar, cvc);
+
         Assert.assertTrue(result);
+
     }
+
+
 
 }
