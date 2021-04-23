ba772cc21f81d33bb10172e3bbfb217f74ba3c59
chandler-lee <13106126@163.com>
2021-04-23 11:28:40 +0800

    描述:mecm-inventory挂接servicecomb 解决问题:mecm-inventory挂接servicecomb

--- src/main/java/org/edgegallery/mecm/inventory/InventoryApplication.java
+++ src/main/java/org/edgegallery/mecm/inventory/InventoryApplication.java
@@ -53,21 +53,6 @@ public class InventoryApplication {
     private String trustStorePasswd;
 
     /**
-     * Returns new instance of restTemplate with required configuration.
-     *
-     * @return restTemplate with required configuration
-     */
-    // @Bean
-    // public RestTemplate restTemplate() {
-    //     RestClientHelper builder =
-    //             new RestClientHelper(Boolean.parseBoolean(isSslEnabled), trustStorePath, trustStorePasswd);
-    //     CloseableHttpClient client = builder.buildHttpClient();
-    //     HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(client);
-    //     factory.setBufferRequestBody(false);
-    //     return new RestTemplate(factory);
-    // }
-
-    /**
      * External system Inventory entry function.
      *
      * @param args arguments
