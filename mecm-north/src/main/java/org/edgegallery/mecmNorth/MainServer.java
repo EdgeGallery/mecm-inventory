76212b83d89d773d128de35d11419e042e06f70c
SeanGao123 <gaoguozhen@huawei.com>
2021-12-14 09:34:56 +0800

    mecm北向接口创建

---
+++ mecm-north/src/main/java/org/edgegallery/mecmNorth/MainServer.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.edgegallery.mecmNorth;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+@SpringBootApplication(scanBasePackages = "org.edgegallery.mecmNorth", exclude = {SecurityAutoConfiguration.class})
+@EnableScheduling
+@EnableServiceComb
+public class MainServer {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(MainServer.class);
+
+    /**
+     * Main.
+     */
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException {
+        // do not check host name
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+        SSLContext sc = SSLContext.getInstance("TLSv1.2");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+        SpringApplication.run(MainServer.class, args);
+
+    }
+}
