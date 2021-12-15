c3b7dcd751887ab2b7d6e0d30a90e9736db8243c
chandler-lee <13106126@163.com>
2021-12-15 09:53:38 +0800

    north增加定时任务

--- mecm-north/src/main/java/org/edgegallery/mecmNorth/facade/scheduleiml/ScheduleInstantiateImpl.java
+++ mecm-north/src/main/java/org/edgegallery/mecmNorth/facade/scheduleiml/ScheduleInstantiateImpl.java
@@ -4,6 +4,7 @@ import static org.edgegallery.mecmNorth.utils.constant.Constant.APP_NAME;
 import static org.edgegallery.mecmNorth.utils.constant.Constant.APP_VERSION;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import org.edgegallery.mecmNorth.model.MecMPackageInfo;
 import org.edgegallery.mecmNorth.repository.mapper.MecMDeploymentMapper;
 import org.edgegallery.mecmNorth.repository.mapper.MecMPackageMapper;
 import org.edgegallery.mecmNorth.service.MecmService;
+import org.edgegallery.mecmNorth.utils.InitParamsUtil;
 import org.edgegallery.mecmNorth.utils.constant.Constant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,7 +59,7 @@ public class ScheduleInstantiateImpl {
         packageInfo.put(APP_NAME, mecmPkg.getMecmPkgName());
         packageInfo.put(APP_VERSION, mecmPkg.getMecmPkgVersion());
 
-        Map<String, Object> paramsMap = handleParams(subJob.getParams());
+        Map<String, Object> paramsMap = InitParamsUtil.handleParams(subJob.getParams());
 
         // instantiate original app
         String appInstanceId = mecmService.createInstanceFromAppoOnce(context, subJob.getMecmPkgName()
@@ -66,14 +68,12 @@ public class ScheduleInstantiateImpl {
 
         MecMPackageDeploymentInfo infoGetFromApm = MecMPackageDeploymentInfo.builder().id(subJob.getId()).
             mecmPackageId(subJob.getMecmPackageId()).mecmPkgName(subJob.getMecmPkgName()).hostIp(subJob
-            .getHostIp()).statusCode(5).appInstanceId(appInstanceId).status(Constant.INSTANTIATING_STATUS).build();
+            .getHostIp()).statusCode(Constant.STATUS_INSTANTIATING).appInstanceId(appInstanceId).status(Constant
+            .INSTANTIATING_STATUS).build();
         mecMDeploymentMapper.updateMecmPkgDeploymentInfo(infoGetFromApm);
     }
 
-    private Map<String, Object> handleParams(String params) {
-        //TODO: 参数转化，将数据库存储实例化参数
-        return new HashMap<>();
-    }
+
 
     public void queryInstantiate(MecMPackageDeploymentInfo subJob) {
         Map<String, String> context = new HashMap<>();
@@ -96,13 +96,21 @@ public class ScheduleInstantiateImpl {
         context.put(Constant.APP_INSTANCE_ID, subJob.getAppInstanceId());
 
         String status = mecmService.getApplicationInstanceOnce(context, subJob.getAppInstanceId());
-
+        MecMPackageDeploymentInfo infoGetFromApm;
         if(!status.equals(Constant.INSTANTIATED_STATUS) && !status.equals(Constant.INSTANTIATING_STATUS)) {
+            LOGGER.error("Error happens when instantiating.");
             status = Constant.INSTANTIATE_ERROR_STATUS;
+            infoGetFromApm = MecMPackageDeploymentInfo.builder().id(subJob.getId()).
+                mecmPackageId(subJob.getMecmPackageId()).mecmPkgName(subJob.getMecmPkgName()).hostIp(subJob
+                .getHostIp()).statusCode(Constant.STATUS_ERROR).appInstanceId(subJob.getAppInstanceId()).status(status).build();
+        } else {
+            LOGGER.error("package status finished. package id is: " + subJob.getMecmPackageId());
+            status = Constant.FINISHED_STATUS;
+            infoGetFromApm = MecMPackageDeploymentInfo.builder().id(subJob.getId()).
+                mecmPackageId(subJob.getMecmPackageId()).mecmPkgName(subJob.getMecmPkgName()).hostIp(subJob
+                .getHostIp()).statusCode(Constant.STATUS_FINISHED).appInstanceId(subJob.getAppInstanceId()).status(status).build();
         }
-        MecMPackageDeploymentInfo infoGetFromApm = MecMPackageDeploymentInfo.builder().id(subJob.getId()).
-            mecmPackageId(subJob.getMecmPackageId()).mecmPkgName(subJob.getMecmPkgName()).hostIp(subJob
-            .getHostIp()).statusCode(5).appInstanceId(subJob.getAppInstanceId()).status(status).build();
+
         mecMDeploymentMapper.updateMecmPkgDeploymentInfo(infoGetFromApm);
     }
 }
