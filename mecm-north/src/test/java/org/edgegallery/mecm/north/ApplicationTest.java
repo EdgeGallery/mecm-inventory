744d29d19d999a7ac8a96173afc7737d141b0180
SeanGao123 <gaoguozhen@huawei.com>
2021-12-15 21:40:13 +0800

    add testcase

---
+++ mecm-north/src/test/java/org/edgegallery/mecm/north/ApplicationTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.edgegallery.mecm.north;
+
+/*
+@SpringBootApplication(scanBasePackages = "org.edgegallery.mecm.north", exclude = {SecurityAutoConfiguration.class})
+@MapperScan(basePackages = {"org.edgegallery.mecm.north.repository.mapper"})
+@EnableScheduling
+@EnableServiceComb
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTest.class, args);
+    }
+
+    @Bean
+    public OncePerRequestFilter accessTokenFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                FilterChain filterChain) throws ServletException, IOException {
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+}
+*/
+
