998948a57479c7f0537e1626c14b4c18872652fd
SeanGao123 <gaoguozhen@huawei.com>
2021-12-16 19:20:57 +0800

    fix bugs

--- mecm-north/src/main/java/org/edgegallery/mecm/north/controller/filter/AccessTokenFilter.java
+++ mecm-north/src/main/java/org/edgegallery/mecm/north/controller/filter/AccessTokenFilter.java
@@ -18,14 +18,15 @@
 package org.edgegallery.mecm.north.controller.filter;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
-import org.edgegallery.mecm.north.utils.constant.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerTokenServicesConfiguration;
 import org.springframework.context.annotation.Import;
@@ -38,82 +39,80 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 @Component
 @Import({ResourceServerTokenServicesConfiguration.class})
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AccessTokenFilter extends OncePerRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessTokenFilter.class);
+    private static final String INVALID_TOKEN_MESSAGE = "Invalid access token";
+    private static final String[] HEALTH_URI = {"/north/v1/health"};
+
     @Autowired
     TokenStore jwtTokenStore;
-
-    public static final ThreadLocal<Map<String, String>> CONTEXT = new ThreadLocal<>();
-
-    private static final String[] NO_NEED_TOKEN_URLS = {"GET /health"};
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
-        if (!isNoNeedToken(request)) {
-            String accessTokenStr = request.getHeader(Constant.ACCESS_TOKEN);
-            if (StringUtils.isEmpty(accessTokenStr)) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token is empty");
-                return;
-            }
-            OAuth2AccessToken accessToken = jwtTokenStore.readAccessToken(accessTokenStr);
-            if (accessToken == null) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), Constant.INVALID_ACCESS_TOKEN);
-                return;
-            }
-            if (accessToken.isExpired()) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token expired");
-                return;
-            }
-            Map<String, Object> additionalInfoMap = accessToken.getAdditionalInformation();
-            if (additionalInfoMap == null) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), Constant.INVALID_ACCESS_TOKEN);
-                return;
-            }
-            String userIdFromRequest = request.getParameter(Constant.USER_ID);
-            String userIdFromToken = additionalInfoMap.get(Constant.USER_ID).toString();
-            if (!StringUtils.isEmpty(userIdFromRequest) && !userIdFromRequest.equals(userIdFromToken)) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal userId");
-                return;
-            }
-            String userNameFromRequest = request.getParameter(Constant.USER_NAME);
-            String userNameFromToken = additionalInfoMap.get(Constant.USER_NAME).toString();
-            if (!StringUtils.isEmpty(userNameFromRequest) && !userNameFromRequest.equals(userNameFromToken)) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal userName");
-                return;
-            }
-            OAuth2Authentication auth = jwtTokenStore.readAuthentication(accessToken);
-            if (auth == null) {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), Constant.INVALID_ACCESS_TOKEN);
-                return;
-            }
-            Map<String, String> contextMap = new HashMap<>();
-            contextMap.put(Constant.ACCESS_TOKEN, accessTokenStr);
-            contextMap.put(Constant.USER_ID, userIdFromToken);
-            contextMap.put(Constant.USER_NAME, userNameFromToken);
-            CONTEXT.set(contextMap);
-
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        // Skip token check for health check URI
+        if (request.getRequestURI() != null && (Arrays.stream(HEALTH_URI)
+            .anyMatch(request.getRequestURI()::contains))) {
+            filterChain.doFilter(request, response);
+            return;
         }
+
+        String accessTokenStr = request.getHeader("access_token");
+        if (StringUtils.isEmpty(accessTokenStr)) {
+            LOGGER.error("Access token is empty.");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Access token is empty");
+            return;
+        }
+
+        OAuth2AccessToken accessToken = jwtTokenStore.readAccessToken(accessTokenStr);
+        if (accessToken == null || accessToken.isExpired()) {
+            LOGGER.error("Access token has expired.");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), INVALID_TOKEN_MESSAGE);
+            return;
+        }
+
+        Map<String, Object> additionalInfoMap = accessToken.getAdditionalInformation();
+        OAuth2Authentication auth = jwtTokenStore.readAuthentication(accessToken);
+        if (additionalInfoMap == null || auth == null) {
+            LOGGER.error("Access token is invalid.");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), INVALID_TOKEN_MESSAGE);
+            return;
+        }
+
+        if (additionalInfoMap.get("userId") == null) {
+            LOGGER.error("Access token is invalid.");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), INVALID_TOKEN_MESSAGE);
+            return;
+        }
+        String userIdFromToken = additionalInfoMap.get("userId").toString();
+        String userIdFromRequest = getTenantId(request.getRequestURI());
+
+        if (userIdFromRequest != null
+            && !StringUtils.isEmpty(userIdFromRequest) && !userIdFromRequest.equals(userIdFromToken)) {
+            LOGGER.error("Illegal tenant ID");
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Illegal tenant ID");
+            return;
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
         filterChain.doFilter(request, response);
     }
 
-    public static void deleteContext() {
-        CONTEXT.remove();
-    }
-
-    private boolean isNoNeedToken(HttpServletRequest request) {
-        if (request.getRequestURI() == null) {
-            return true;
-        }
-        String accessUrl = String.format("%s %s", request.getMethod(), request.getRequestURI());
-        for (String filter : NO_NEED_TOKEN_URLS) {
-            if (accessUrl.matches(filter)) {
-                return true;
+    private String getTenantId(String url) {
+        if (url != null) {
+            String[] parts = url.split("/");
+            int indexOfTenant = Arrays.asList(parts).indexOf("tenants");
+            if (indexOfTenant != -1 && parts.length >= indexOfTenant) {
+                return parts[indexOfTenant + 1];
             }
         }
-        return false;
+        return null;
     }
 }
+
