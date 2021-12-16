8740341cbb5cff400fcb2816281f17fa5ed12a22
SeanGao123 <gaoguozhen@huawei.com>
2021-12-16 19:44:57 +0800

    fix AdminId

--- mecm-north/src/main/java/org/edgegallery/mecm/north/controller/MecHostController.java
+++ mecm-north/src/main/java/org/edgegallery/mecm/north/controller/MecHostController.java
@@ -47,6 +47,8 @@ public class MecHostController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MecHostController.class);
 
+    private static final String AdminId = "39937079-99fe-4cd8-881f-04ca8c4fe09d";
+
     @Autowired
     private MecmHostServiceFacade mecmHostServiceFacade;
 
@@ -62,7 +64,7 @@ public class MecHostController {
     public ResponseEntity<ResponseObject> queryMecmHosts(HttpServletRequest httpServletRequest) {
         LOGGER.info("enter query mecm hosts.");
         return mecmHostServiceFacade.getAllMecmHosts((String) httpServletRequest.getAttribute(Constant.ACCESS_TOKEN),
-            (String) httpServletRequest.getAttribute(Constant.USER_ID));
+            AdminId);
     }
 
 }
